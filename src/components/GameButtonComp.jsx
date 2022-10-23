3bb263bcd7961573db7bf7bc052ddf59006e1896
aalole <abiodunrasheed93@gmail.com>
2022-10-22 23:44:06 -0400

    added play game functionality to the tik-tak-toe game

--- src/components/GameButtonComp.jsx
+++ src/components/GameButtonComp.jsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const GameButtonComp = () => {
-    const [buttons, setButtons] = useState(Array.from(Array(9).fill('')));
-    
+const GameButtonComp = ({buttons, draw}) => {    
     return (
         <div className='h-auto flex flex-wrap w-[561px] my-2 mx-auto pl-5'>
             {
                 buttons.map((el, i) => (
-                    <button key={i} className='w-[130px] h-[130px] my-5 mx-5 bg-[#1F3641] rounded-lg' style={{boxShadow:  'inset 0px -8px 0px #10212A'}}></button>
+                    <button key={i} className={`w-[130px] h-[130px] my-5 mx-5 bg-[#1F3641] rounded-lg text-lg font-bold text-gray`} style={{boxShadow:  'inset 0px -8px 0px #10212A'}} onClick={(e)=> draw(e, i+1)}></button>
                 ))
             }
         </div>
